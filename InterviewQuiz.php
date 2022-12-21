d078f20a8091f221e74d088d29f989d6038bef4a
wan kei jung <jung6241@hotmail.com>
2022-12-21 21:47:31 +0800

    InterviewQuiz

--- InterviewQuiz.php
+++ InterviewQuiz.php
@@ -7,6 +7,14 @@ $k = 3;
 rotate($nums,$k);
 print_r($nums); // output the result
 
+
+// Example 2
+$nums = [-1,-100,3,99];
+$k = 2;
+
+rotate($nums,$k);
+print_r($nums); // output the result
+
 function rotate(&$nums,$k){
     do{
         $pop = array_pop($nums); // get & delete the last number from $nums
@@ -18,19 +26,4 @@ function rotate(&$nums,$k){
 }
 
 
-// Example 2
-$nums2 = [-1,-100,3,99];
-$k2 = 2;
 
-rotate2($nums2,$k2);
-print_r($nums2); // output the result
-
-function rotate2(&$nums2,$k2){
-    do{
-        $pop2 = array_pop($nums2); // get & delete the last number from $nums
-        array_unshift($nums2,$pop2); // use $pop as the insert number
-        $k2--;  // count the steps
-
-    }while($k2>0);
-
-}
